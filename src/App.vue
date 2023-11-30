e2ec09f8cdc28dcce6173a847a32de93afc39ea8
Lucas Gismondi <lucas@gismondigroup.com>
2023-11-30 10:20:04 -0500

    adds layer sdk

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { initLayerBuilder } from '@buildwithlayer/builder';
+import { initLayerChat } from '@buildwithlayer/chat';
+import { initLayerCore } from '@buildwithlayer/core';
+
+initLayerBuilder();
+initLayerChat();
+initLayerCore();
+
+layerBuilder.mount();
+layerChat.mount();
+layerCore.mount({
+    copilot: 'RAG',
+    systemMessage:
+        'Please be precise in your answers. If your answer requires bullet points, please also be short and to the point.',
+    layerApiKey: '16b4bc8c-6948-4f88-931b-21918b9a5178',
+
+    // Do not include this in production code
+    layerCustomerApiKey: 'aea81193-1090-4294-b57f-9aa073e0c364',
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <img
+            src="https://www.smartxadvisory.com/wp-content/uploads/2020/09/smartx-logo.png"
+            class="logo vue"
+            alt="Vue logo"
+        />
+    </div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
---
+++ src/globals.d.ts
@@ -0,0 +1,3 @@
+declare const layerBuilder;
+declare const layerChat;
+declare const layerCore;
